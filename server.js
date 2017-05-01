615fcff6a0df42bac222451442ac30cb213e68e5
jonotko <j.mastermind1010@gmail.com>
2017-05-01 20:15:35 +0000

    Complete timestamp

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 var express = require("express");
-
+var moment = require("moment");
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get("/:string", function(req, res){
+    
+  var string =  req.params.string; 
+  var timeObject;
+  if(!isNaN(parseInt(string))){
+      var time = moment(parseInt(string)*1000).format("MMMM DD, YYYY");
+
+      timeObject = {
+          unix: string,
+          natural: time
+      }
+  }else if (moment(string).isValid()){
+      
+      var time = moment(string).format("X");
+      
+      timeObject = {
+          unix: time,
+          natural: string
+      }
+  }else{
+      
+      timeObject = {
+          unix: null,
+          natural: null
+      }
+  }
+  
+   res.send(JSON.stringify(timeObject));
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
